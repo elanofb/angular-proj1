0e3c2d55a91d89ef3a8cf25e568a92009604c21d
elanofb <elanofb@gmail.com>
2020-12-07 19:20:37 -0300

    Update app.e2e-spec.ts

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to booking-creation-guide-frontend!');
+    expect(page.getParagraphText()).toEqual('Welcome to app teste');
   });
 });
